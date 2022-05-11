5a76df4a2656c8342a7b7b51d30069d53c94f971
syugi <altnr23@gmail.com>
2022-05-11 17:56:54 +0900

    add 소셜로그인 기능추가

---
+++ src/main/java/com/msheo/springbootawsstudy/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.msheo.springbootawsstudy.config.auth;
+
+import com.msheo.springbootawsstudy.domain.Role;
+import com.msheo.springbootawsstudy.service.Custom0Auth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final Custom0Auth2UserService custom0Auth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                .userService(custom0Auth2UserService);
+    }
+}
--- src/main/java/com/msheo/springbootawsstudy/controller/IndexController.java
+++ src/main/java/com/msheo/springbootawsstudy/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.msheo.springbootawsstudy.controller;
 
+import com.msheo.springbootawsstudy.config.auth.LoginUser;
+import com.msheo.springbootawsstudy.config.auth.dto.SessionUser;
 import com.msheo.springbootawsstudy.dto.PostsResponseDto;
 import com.msheo.springbootawsstudy.service.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
