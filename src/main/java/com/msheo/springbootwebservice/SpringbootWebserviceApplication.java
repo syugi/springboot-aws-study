c9dc6514d5febe373dfecc0e70c55c97361ccbe3
syugi <altnr23@gmail.com>
2022-05-31 15:21:21 +0900

    project name change

--- src/main/java/com/msheo/springbootwebservice/SpringbootWebserviceApplication.java
+++ src/main/java/com/msheo/springbootwebservice/SpringbootWebserviceApplication.java
@@ -3,7 +3,6 @@ package com.msheo.springbootwebservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootWebserviceApplication {
 
