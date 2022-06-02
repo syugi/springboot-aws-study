b034dbd210738719e9d238423592a5a4d0c430d8
syugi <altnr23@gmail.com>
2022-06-02 13:16:10 +0900

    테스트

--- src/test/java/com/msheo/springbootwebservice/controller/IndexControllerTest.java
+++ src/test/java/com/msheo/springbootwebservice/controller/IndexControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
@@ -18,10 +19,10 @@ public class IndexControllerTest {
 
     @Test
     public void 메인페이지_로딩() {
-//        //when
-//        String body = this.restTemplate.getForObject("/", String.class);
-//
-//        //then
-//        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
